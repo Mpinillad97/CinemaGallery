a9684c1c0e537d9ef38c58f0400d472f15f34a07
MPinillaDavila <mpinillad97@gmail.com>
2024-09-25 19:10:16 -0500

    Primera tarea y adicion de estructura cinema

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,1 +1,23 @@
-console.log(tempData);
+const movieContainer = document.querySelector('.movieContainer');
+
+//Funcion para crear cada tarjeta
+function createMovieCard(movie) {
+    return `
+    <div class="card">
+      <div class="movie-info">
+        <a href="#" class="movie-title">${movie.title}</a> <!-- Enlace vacío -->
+      </div>
+      <img src="${movie.poster}" alt="${movie.title}" class="movie-poster">
+      <div class="movie-details">
+        <p><strong>Año:</strong> ${movie.year}</p> <!-- Año de la película aquí -->
+        <p><strong>Director:</strong> ${movie.director}</p>
+        <p><strong>Duration:</strong> ${movie.duration}</p>
+        <p><strong>Genre:</strong> ${movie.genre.join(', ')}</p>
+        <div class="rating-box"><strong>Rating:</strong> ${movie.rate}</div>
+      </div>
+    </div>
+  `;
+}
+
+
+movieContainer.innerHTML = tempData.map(createMovieCard).join('');
--- front/scripts/tempData.js
+++ front/scripts/tempData.js
@@ -29,4 +29,34 @@ const tempData = [
     poster:
       "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg",
   },
+  {
+    title: "Harry Potter and the Deathly Hallows: Part 2",
+    year: 2011,
+    director: "David Yates",
+    duration: "2h 10min",
+    genre: ["Adventure", "Drama", "Fantasy"],
+    rate: 8.1,
+    poster:
+      "https://i.ebayimg.com/images/g/k34AAOSw3ydV35lN/s-l1200.jpg",
+  },
+  {
+    title: "The Godfather",
+    year: 1972,
+    director: "Francis Ford Coppola",
+    duration: "2h 55min",
+    genre: ["Crime", "Drama"],
+    rate: 9.2,
+    poster:
+      "https://hips.hearstapps.com/hmg-prod/images/gabz-godfather-1646152137.png",
+  },
+  {
+    title: "Ready Player One",
+    year: 2018,
+    director: "Steven Spielberg",
+    duration: "2h 20min",
+    genre: ["Action", "Adventure", "Sci-Fi"],
+    rate: 7.4,
+    poster:
+      "https://image.tmdb.org/t/p/original/2iuVrtC5IpwLtSFSgkIIIKLs0Zq.jpg",
+  }
 ];
