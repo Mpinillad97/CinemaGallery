0a473cfe6ea4960e3fba8eb91103c877050f5232
MPinillaDavila <mpinillad97@gmail.com>
2024-10-26 15:09:44 -0500

    adicion de verificaciones al formulario de crear Peliculas

--- front/scripts/form.js
+++ front/scripts/form.js
@@ -1,35 +1,33 @@
 const form = document.querySelector("form");
+const clearForm = document.querySelector("#clearForm");
 
 form.addEventListener("submit", (e) => {
     e.preventDefault()
-
-
-    
-   
+    const genresChecked = document.querySelectorAll('input[type="checkbox"]:checked');
+    const genresList = Array.from(genresChecked).map(checkbox => checkbox.value);
     const objetoMovie = {
         title: form.title.value,
         year: form.year.value,
         director: form.director.value,
         duration: `${form.hour.value} h ${form.minutes.value} m`,
-        rating: form.rating.value,
-        posterURL: form.posterUrl.value
+        genre: genresList,
+        rate: form.rate.value,
+        poster: form.posterUrl.value
     }
-
     for(const key in objetoMovie){
         if(!objetoMovie[key]){
             alert(`El campo ${key} es requerido`)
+            return
         }
-
-    if(objetoMovie["genre"].length === 0){
-        alert(`${genre}`)
+        if(objetoMovie["genre"].length === 0){
+            alert(`Debe seleccionar minimo un genero`)
+            return
         }
     }
-
     console.log(objetoMovie);
 })
 
-form.addEventListener("reset", () => {
-    e.preventDefault();
-
-    
+clearForm.addEventListener("click", () => {
+    form.reset()
 })
+
