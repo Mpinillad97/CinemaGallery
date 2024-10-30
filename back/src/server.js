f2439666a2d6899aa2e270b38ae4fe43a6cdc8d0
MPinillaDavila <mpinillad97@gmail.com>
2024-10-29 21:55:17 -0500

    modificaciones en el server

--- back/src/server.js
+++ back/src/server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const router = require("./routes/index")
 const morgan = require("morgan");
 const cors = require("cors");
 
@@ -9,7 +8,7 @@ app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 
-app.use(router);
+app.use(require("./routes"));
 
 module.exports = app;
 
